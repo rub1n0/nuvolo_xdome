74ff81d457b264897092c0ae07df8df57422afea
Anthony Rubino <>
2025-07-31 13:53:51 -0700

    developing engine

--- engine.py
+++ engine.py
@@ -6,12 +6,24 @@ import Claroty_Client as CC
 cmms = NC.NuvoloClient()
 xdome = CC.ClarotyClient()
 
-devices_cmms = cmms.search_devices(limit=1000, u_scripps_bio_active="true")
-for device in devices_cmms.json().get("result", []):
-    # print(f"{device.get('name')}")
-    devices_xdome = xdome.get_devices(fields="asset_id", filter_by={
-        "cmms_asset_tag": device.get('name'),
-    }).json().get("result", [])
-    if devices_xdome:
-        print(f"  Found in XDome: {devices_xdome[0].get('asset_id')}")
+matched_device_count = 0
 
+import concurrent.futures
+
+devices_cmms = cmms.search_devices(limit=300, u_scripps_bio_active="true")
+devices_list = devices_cmms.json().get("result", [])
+
+def process_device(device):
+    beic = device.get('name')
+    device_xdome = xdome.get_devices(filter_by={"cmdb_asset_tag": beic}, fields=["name", "ip_address"])
+    if device_xdome.status_code == 200:
+        if device_xdome:
+            print(f"Device {beic} found in XDome")
+            return 1
+    return 0
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = list(executor.map(process_device, devices_list))
+    matched_device_count = sum(results)
+
+print(f"Total matched devices: {matched_device_count}")
